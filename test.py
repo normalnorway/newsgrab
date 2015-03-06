4e95ff01be7ef7ea0edc9346fbe8fe8471515e99
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-03-06 23:44:20 +0100

    addedd test.py

---
+++ test.py
@@ -0,0 +1,55 @@
+"""
+Used while developing to simplify testing.
+
+$ python test.py <filename>
+$ python test.py <url>
+"""
+
+import sys, os
+from urlparse import urlsplit
+from driver import domain_to_parser_id
+
+# Some sites requires a Norwegian locale for the date parsing
+# functions to work. Dagsavisen,
+import locale
+locale.setlocale (locale.LC_TIME, 'nb_NO.utf8')
+
+
+# Load file or url
+try:
+    filename = sys.argv[1]
+    fp = open (filename)
+    base,ext = os.path.splitext (os.path.basename (filename))
+    parser_id = base
+except IOError:
+    import urllib2
+    url = sys.argv[1]
+    fp = urllib2.urlopen (url)
+    parser_id = domain_to_parser_id (urlsplit(url).hostname)
+except IndexError:
+    exit ('Usage: %s <url>|<filename>' % sys.argv[0])
+
+
+# Load parser
+try:
+    mod = __import__ ('parsers.'+parser_id, fromlist=['Parser'])
+    parser = mod.Parser()
+except ImportError as ex:
+    print >>sys.stderr, "WARNING: No custom parser. Using OpenGraphParser"
+    from parsers import OpenGraphParser
+    parser = OpenGraphParser()
+
+#if filename:
+#    parser.set_html (fp.read())
+#elif url:
+#    parser.set_url (url)
+
+parser.set_html (fp.read())
+data = parser.get()
+#from pprint import pprint
+#pprint (data)
+
+for key,val in data.iteritems():
+    print '# %s\n[%s]\n' % (key.ljust(8), val)
+    #print '%s <%s>' % (key.ljust(8), val)
+    #print key.ljust(10), val
