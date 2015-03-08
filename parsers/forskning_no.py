ccde851f9f4cd9fd762c359ef953ce6b1acad14c
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-03-08 23:44:35 +0100

    bugfix: forskning.no

--- parsers/forskning_no.py
+++ parsers/forskning_no.py
@@ -2,13 +2,14 @@
 '''
 forskning.no
 
-<link rel="canonical" href="http://forskning.no/2014/10/hvor-farlig-er-cannabisbruk" />
+<html  lang="nb" dir="ltr" prefix="og: http://ogp.me/ns#">
 
-<meta property="og:image" content="http://forskning.no/sites/forskning.no/files/201052_hasj_None.jpg" />
-<meta property="og:title" content="Slik skader cannabis" />
+Missing: og:description
 
-@note not equal to ingress
-<meta name="twitter:description" content="Om cannabis skal legaliseres har vært et hett debattema i mange land den siste tiden, også i Norge. Nå har den australske forskeren Wayne Hall gått igjennom mye av forskningen siden 1993 som tar for seg usunne og helseskadelige konsekvenser av cannabisbruk.  Oversikten til Wayne Hall tar både for seg negative konsekvenser av kronisk bruk av cannabis, og eventuelle konsekvenser av sjeldent bruk, eller bare å bruke cannabis én gang. Ifølge Halls forskning øker cannabisbruk blant annet risikoen for alvorlige ulykker og psykiske lidelser som schizofreni." />
+<link rel="canonical" href="..." />
+
+Note: Not equal to "ingress"
+<meta name="twitter:description" content="..." />
 
 div#content
 h1.title
@@ -17,39 +18,21 @@ div.field-published-date
 '''
 
 from datetime import datetime
-from . import ParserBase
+from . import OpenGraphParser
 
-
-def getmetaprop (tree, key):
-    return tree.xpath("/html/head/meta[@property='%s']/@content" % key)[0]
-
-
-class Parser (ParserBase):
-    def __str__ (self):
-        return 'abcnyheter.no'
+class Parser (OpenGraphParser):
 
     def parse (self):
-        data = dict()
-        tree = self.tree
+        meta = super(Parser,self).parse()
+        xpath = self.tree.xpath
 
-        tmp = tree.xpath('//div[contains(@class, "field-name-published-date")]')[0]
+        # Date
+        tmp = xpath('//div[contains(@class, "field-name-published-date")]')[0]
         tmp = tmp.xpath("div/div")[0].text.strip()
-        data['date'] = datetime.strptime(tmp, '%d.%m %Y %H:%M')
+        meta['date'] = datetime.strptime(tmp, '%d.%m %Y %H:%M')
 
-        data['title'] = getmetaprop (tree, 'og:title')
+        # Description
+        tmp = xpath('//div[contains(@class, "field-name-field-intro-text")]')[0]
+        meta['description'] = tmp.xpath("div/div/p")[0].text.strip()
 
-        tmp = tree.xpath('//div[contains(@class, "field-name-field-intro-text")]')[0]
-        data['summary'] = tmp.xpath("div/div/p")[0].text.strip()
-
-        data['image'] = getmetaprop (tree, 'og:image')
-
-        return data
-
-
-'''
-def getdiv (elem, key, subpath='div/div', extra=None):
-    tmp = elem.xpath('//div[contains(@class, "field-%s")]' % key)[0]
-    if extra: subpath += '/' + extra
-    return tmp.xpath (subpath)[0].text.strip()
-print getdiv (tree, 'name-field-intro-text')
-'''
+        return meta
