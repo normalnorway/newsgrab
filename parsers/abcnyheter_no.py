bd158df248a3fd13c85efde5f622de720ea51bf4
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-03-16 04:29:51 +0100

    simplified abcnyheter

--- parsers/abcnyheter_no.py
+++ parsers/abcnyheter_no.py
@@ -8,6 +8,18 @@ Supports schema.org:
 - http://schema.org/ImageObject
 - http://schema.org/Person  (Author)
 
+Meta:   title, description, image, type, url, site_name
+
+Some interesting itemprops:
+  1  datePublished
+  1  articleBody
+  1  author
+  1  articleSection
+  1  headline
+  6  image      ?? is the first image the correct one?
+  1  keywords
+  1  text       ?? what is this for?
+
 # Other usefull elements:
 <link rel="canonical" href="..." />
 <meta name="keywords"      content="comma separated keyword list" />
@@ -18,7 +30,4 @@ Supports schema.org:
 from . import OpenGraphParser
 
 class Parser (OpenGraphParser):
-    def parse (self):
-        meta = super(Parser,self).parse()
-        meta['date'] = self.parse_date()
-        return meta
+    pass
