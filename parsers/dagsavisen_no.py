bd4f2a0b3b31f2bbe295c24b51cecbda638a743e
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-03-16 06:16:21 +0100

    dagsavisen: use norwegian dateparser

--- parsers/dagsavisen_no.py
+++ parsers/dagsavisen_no.py
@@ -1,33 +1,28 @@
-''' abcnyheter.no
+""" dagsavisen.no
 
 Supports Open Graph without using the namespace.
 
 Only provides the published date, not the time :(
-Must parse date manually, datePublished itemprop not supported
 
-Note: Uses Norwegian month names, so need date parser that handles this.
+Must parse date manually, datePublished itemprop not supported.
+
+Date example: Publisert 5. mars 2015
 
 <article class="large" itemscope="" itemtype="http://schema.org/NewsArticle" role="article">
-<link rel="canonical" href="http://www.dagsavisen.no/verden/mulig-vendepunkt-i-irak-1.320192">
-'''
+<link rel="canonical" href="...">
+"""
 
 from . import OpenGraphParser
-from datetime import datetime
 
 class Parser (OpenGraphParser):
+
     def parse_date (self):
         expr = "//article[@role='article']/div[@class='byline']//div[@class='time']/span/text()"
         tmp = self.tree.xpath (expr)
         assert len(tmp)==1
         datestr = tmp[0].strip()
-        #return datetime.strptime (datestr, 'Publisert %d. %B %Y')
-        # XXX: tmp hack
-        import locale
-        oldloc = locale.getlocale()
-        locale.setlocale(locale.LC_TIME, "nb_NO.UTF-8")
-        date = datetime.strptime (datestr, 'Publisert %d. %B %Y')
-        locale.setlocale(locale.LC_TIME, '.'.join(oldloc) if oldloc[0] else "C")
-        return date
+        datestr = datestr.split(' ', 1)[-1]     # remove first word
+        return self.parse_date_no (datestr)
 
     def parse (self):
         meta = super(Parser,self).parse()
