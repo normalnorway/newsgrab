dd0140564153a7c3530982581c2739460ff65686
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-09-16 19:17:24 +0200

    dagbladet.no: handle more date formats

--- newsgrab/parsers/dagbladet_no.py
+++ newsgrab/parsers/dagbladet_no.py
@@ -25,20 +25,38 @@ from . import OpenGraphParser
 
 class Parser (OpenGraphParser):
 
+    # <p class="article-byline-category">Publisert  den <strong>16. jan 2015,</strong> kl. 07:00 av</p>
+    def try_parse_date (self):
+        lst = self.body.xpath ('.//p[@class="article-byline-category"]')
+        assert len(lst)==1
+        assert lst[0].text.startswith ('Publisert')
+        L = lst[0].getchildren()
+        assert len(L)==1
+        strong = L[0]
+        assert strong.tag == 'strong'
+        datestr = strong.text[:-1]
+        timestr = strong.tail
+        timestr = timestr.replace('av', '').replace('kl.','').strip()
+        return self.parse_date_no (datestr + ' ' + timestr)
+
     def parse (self):
         meta = super(Parser,self).parse()
 
         # div#articleTools
         # div.article-date > span.date + span.time
         L = self.tree.xpath ("//div[@class='article-date']")
-        assert len(L)==1
-        elem = L[0]
-        e1 = elem.xpath ("span[@class='date']/text()")
-        e2 = elem.xpath ("span[@class='time']/text()")
-        # e1 = ['fredag 30. januar 2015']
-        # e2 = ['kl.10:25']
-        datestr = e1[0].split(' ', 1)[1].strip()    # remove first word
-        timestr = e2[0].split('.')[1].strip()       # remove kl.
-        meta['date'] = self.parse_date_no (datestr + ' ' + timestr)
+
+        if not L:
+            meta['date']= self.try_parse_date()
+        else:
+            assert len(L)==1
+            elem = L[0]
+            e1 = elem.xpath ("span[@class='date']/text()")
+            e2 = elem.xpath ("span[@class='time']/text()")
+            # e1 = ['fredag 30. januar 2015']
+            # e2 = ['kl.10:25']
+            datestr = e1[0].split(' ', 1)[1].strip()    # remove first word
+            timestr = e2[0].split('.')[1].strip()       # remove kl.
+            meta['date'] = self.parse_date_no (datestr + ' ' + timestr)
 
         return meta
