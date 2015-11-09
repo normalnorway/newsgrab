0be418d9bc17cdc4f0b0a7abd4b5ca57d4f56eaa
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-11-09 19:38:53 +0100

    bt: bugfix in date parsing on paied articles

--- newsgrab/parsers/bt_no.py
+++ newsgrab/parsers/bt_no.py
@@ -1,4 +1,10 @@
 """ bt.no -- Bergens Tidende
+
+Some articles uses this date format:
+<time class="published"
+      pubdate="pubdate"
+      datetime="2015-10-17 22:00:22 CEST">...</time>
+
 """
 
 from . import OpenGraphParser
@@ -7,9 +13,23 @@ class Parser (OpenGraphParser):
 
     def parse (self):
         meta = super(Parser,self).parse()
+        if 'date' in meta:
+            return meta
 
-        L = self.body.xpath ("//article/p[@itemprop='description']/text()")
-        assert len(L)==1
-        meta['description'] = L[0].strip()
+        # http://www.bt.no/nyheter/lokalt/Vanskelig-a-avslore-Judas-drap-3461791.html
+        # Note: These are behind a paywall
+        L = self.body.xpath ('//time[@class="published"]/@datetime')
+        assert len(L) == 1
+        date, time, tz = L[0].split()   # '2015-10-17 22:00:22 CEST'
+        assert tz in ('CET', 'CEST')
+        datestr = date + 'T' + time
+        meta['date'] = self.parse_iso_date (datestr)
 
         return meta
+
+        # Note: Might include html in the description. Or is that just a bug?
+        # http://www.bt.no/share/article-3086840.html
+        # Wierd way to clean all html tags
+#        from lxml import etree
+#        div = etree.fromstring ('<div>%s</div>' % meta['description'])
+#        meta['description'] = ''.join (div.xpath('//*/text()'))
