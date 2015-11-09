3eba9e82fff900b6b95ef3f290425bdc9c8919c4
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-11-09 18:58:18 +0100

    aftenposten: bugfix in date parser

--- newsgrab/parsers/aftenposten_no.py
+++ newsgrab/parsers/aftenposten_no.py
@@ -4,34 +4,19 @@
       itemprop="datePublished"
       datetime="2014-07-27T08:59:22Z">27.jul. 2014 10:59</time>
 
-Note: This is not picked up be the open graph parser
-<meta name="og:description" content="...">
+<time class="published"
+      pubdate="pubdate"
+      datetime="2015-05-31T\n21:58+01:00"</time>
 
-Note: NO-BREAK SPACE (0xA0) is used.
+Notes:
+* NO-BREAK SPACE (0xA0) is used.
+* some titles is prefixed with author name. e.g. "| Nils August Andresen"
 
 @todo remove query part of image url?
 image = http://www.aftenposten.no/incoming/article7605814.ece/ALTERNATES/w2048c169/afp000694277.jpg?updated=160620141138
-
-# Output from probe.py
-Meta    site_name, title, url, image, type
-itemprop: multiple elements matches
-
-COUNT   ITEMPROP
-  1     articleBody
-  1     author
-  1     dateModified
-  2     datePublished
-  1     description
-  1     email
-  1     headline
-  2     name
-
-Description  : Missing!
 '''
 
 from . import OpenGraphParser
-
-# Note: some titles is prefixed with author name: | Nils August Andresen
 
 class Parser (OpenGraphParser):
 
@@ -39,13 +24,17 @@ class Parser (OpenGraphParser):
 
     def parse (self):
         meta = super(Parser,self).parse (parse_date=False)
-
+ 
         L = self.body.xpath(".//time[@itemprop='datePublished']/@datetime")
-        assert len(L) in (1,2)
-        if len(L) == 2:
-            assert L[0] == L[1]
-        datestr = L[0]
-        assert datestr[-1] == 'Z'
+        if L:
+            assert len(L) in (1,2)
+            if len(L) == 2: assert L[0] == L[1]
+            datestr = L[0]
+        else:
+            # http://www.aftenposten.no/dodsfall/Nekrolog-Nils-Christie-8039532.html
+            L = self.body.xpath(".//time[@class='published']/@datetime")
+            datestr = L[0].replace ('\n', '')
+
         meta['date'] = self.parse_iso_date (datestr)
 
         return meta
