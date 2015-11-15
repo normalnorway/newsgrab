4c694c638855bc63a92802fc05201c782c416e02
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-11-15 12:30:09 +0100

    bugfix: bt.no

--- newsgrab/parsers/bt_no.py
+++ newsgrab/parsers/bt_no.py
@@ -13,12 +13,19 @@ http://www.bt.no/meninger/kommentar/Mathias_Fischer/Cannabis-i-Canada-3463151.ht
 from . import OpenGraphParser
 
 class Parser (OpenGraphParser):
+    pass
 
     # Note: Only called if parse() is unable to parse the date.
-    def parse_date (self):
-        L = self.body.xpath ('//dd[@class="pubDate"]/time/@datetime')
-        assert len(L) == 1
-        # 2015-10-20 12:12:44 CEST
-        # Note: parse_iso_date do not handle named time zones (CEST)
-        datestr = 'T'.join (L[0].split()[:-1]) # remove last word (and use T as time prefix)
-        return self.parse_iso_date (datestr)
+    # Update: No, this will override the generic date parsing,
+    # unless super() is called.
+#    def parse_date (self):
+#        dt = super (Parser, self).parse_date()
+#        if dt: return dt
+#
+#        L = self.body.xpath ('//dd[@class="pubDate"]/time/@datetime')
+#        if len(L) > 0:
+#            assert len(L) == 1
+#            # 2015-10-20 12:12:44 CEST
+#            # Note: parse_iso_date do not handle named time zones (CEST)
+#            datestr = 'T'.join (L[0].split()[:-1]) # remove last word (and use T as time prefix)
+#            return self.parse_iso_date (datestr)
