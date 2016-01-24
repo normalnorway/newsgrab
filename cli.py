22a53b391e491b640d8a9f66f9989f90d716513a
Torkel Bjørnson-Langen <torkel@innovatio.no>
2016-01-24 20:14:40 +0100

    minor

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@
 """Just used for testing while developing"""
 
 # Hack to prefer local newsgrab
+# UPDATE: not needed when installed in editable mode. pip -e
 import sys, os
 sys.path.insert (0, os.getcwd())
 
