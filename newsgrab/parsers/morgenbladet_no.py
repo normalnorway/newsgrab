f6d7befa7a3fcd1719421681ab2b2888b7049b90
Torkel Bjørnson-Langen <torkel@innovatio.no>
2016-02-10 22:04:10 +0100

    morgenbladet: handle missing description & image

--- newsgrab/parsers/morgenbladet_no.py
+++ newsgrab/parsers/morgenbladet_no.py
@@ -1,8 +1,8 @@
 """ morgenbladet.no
 
-Using: Drupal 7 / ramsalt.com
+Using Drupal 7 / ramsalt.com
 
-Have og:updated_time but not og:created_time
+Have og:updated_time but not og:created_time :(
 
 Note: Some articles are missing fields! Like this one:
 https://morgenbladet.no/ideer/2016/01/keiserens-nye-narkotikapolitikk
@@ -19,3 +19,17 @@ class Parser (OpenGraphParser):
         datetime = ''.join(l).strip()
         time, date = datetime.split(' - ')
         return self.parse_date_no (date + ' ' + time)
+
+    def parse (self):
+        meta = super(Parser,self).parse()
+
+        if not 'description' in meta:
+            self.main = self.body.xpath ('//section[@id="main"]')[0]
+            lst = self.main.xpath ('//div[@class="content-main"]//div[contains(@class, "field-name-body")]/p[1]//text()')
+            meta['description'] = ''.join (lst)
+
+        if not 'image' in meta:
+            meta['image'] = 'https://morgenbladet.no/sites/morgenbladet.no/files/styles/large/public/facebook-cover-image.png'
+            # the best we can do for now ...
+
+        return meta
